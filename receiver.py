9700321221f146b950904a2ef8097481438f4f25
Jack Morrison <jackmorrison1@gmail.com>
2014-05-26 21:57:46 -0400

    Adds receiver.py

---
+++ receiver.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import sys
+import zmq
+import mirrorvideo
+
+
+def connect(ctx):
+    socket = ctx.socket(zmq.SUB)
+    socket.setsockopt(zmq.SUBSCRIBE, '')
+    socket.connect('tcp://{}:1776'.format(host))
+    return socket
+
+
+def receive(host):
+    # Open ZMQ context
+    ctx = zmq.Context()
+
+    socket = connect(ctx)
+
+    # Register callback
+    while True:
+        try:
+            emo = socket.recv()
+            mirrorvideo.play_emotion_video(int(emo))
+        except:
+            socket = connect(ctx)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        host = sys.argv[1]
+    else:
+        host = 'mirror1.local'
+    receive(host)
