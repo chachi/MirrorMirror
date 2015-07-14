9af9738cdacbf526eae15218618c455e562a1228
Jack Morrison <jackmorrison1@gmail.com>
2015-07-13 23:28:25 -0600

    Receiver uses tkinter mainloop and callbacks.

--- receiver.py
+++ receiver.py
@@ -3,6 +3,7 @@
 import sys
 import zmq
 import mirrorvideo
+import time
 
 
 def connect(ctx, host):
@@ -14,29 +15,43 @@ def connect(ctx, host):
 
 
 def receive(host):
+    while not mirrorvideo.blank_screen():
+        time.sleep(10)
+
     # Open ZMQ context
     ctx = zmq.Context()
 
     print "Receiving from {}".format(host)
+    class local:
+        """Inner class to encapsulate nonlocal variables.  Hack around
+        scoping.
 
-    socket = None
-    while socket is None:
+        """
+        socket = None
+    while local.socket is None:
         try:
-            socket = connect(ctx, host)
+            local.socket = connect(ctx, host)
         except Exception as e:
             print "Failed to connect with exception {}, trying again.".format(e)
+    root = mirrorvideo.blank_screen()
 
-    # Register callback
-    while True:
-        mirrorvideo.blank_screen()
-        try:
-            print "Listening to socket"
-            emo = socket.recv()
-            print "received {}".format(emo)
-            mirrorvideo.play_emotion_video(int(emo))
-        except Exception as e:
-            print "Failed with {}. Retrying.".format(e)
-            socket = connect(ctx, host)
+    def poll_events():
+        window = mirrorvideo.blank_screen()
+        if window:
+            window.after(0, poll_events)
+            try:
+                emo = local.socket.recv(flags=zmq.NOBLOCK)
+                print "received {}".format(emo)
+                mirrorvideo.play_emotion_video(int(emo))
+            except zmq.ZMQError as e:
+                if e.errno == zmq.EAGAIN:
+                    return
+                else:
+                    print "Failed with {}. Retrying.".format(e)
+                    local.socket = connect(ctx, host)
+
+    root.after(0, poll_events)
+    root.mainloop()
 
 
 if __name__ == '__main__':
