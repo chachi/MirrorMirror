f9e420c3abf726e31f971dddb6d87e7f3ec8ab92
Jack Morrison <jackmorrison1@gmail.com>
2015-07-18 19:06:26 -0600

    Update image after receive.

--- receiver.py
+++ receiver.py
@@ -52,10 +52,6 @@ def receive(host):
     def poll_events():
         window = mirrorvideo.OverlayWindow.create()
 
-        now = datetime.now()
-        if (now - local.last_update).total_seconds() > IMAGE_TIMEOUT:
-            mirrorvideo.blank_screen()
-            local.last_update = now
         if window:
             window.after(0, poll_events)
             try:
@@ -71,6 +67,10 @@ def receive(host):
                 else:
                     lg.info("Failed with {}. Retrying.".format(e))
                     local.socket = connect(ctx, host)
+        now = datetime.now()
+        if (now - local.last_update).total_seconds() > IMAGE_TIMEOUT:
+            mirrorvideo.blank_screen()
+            local.last_update = now
 
     root.after(0, poll_events)
     root.mainloop()
